f923ab24988c01ef72440015943c4c18ae47ee48
GH0ST-141 <151105998+GH0ST-141@users.noreply.github.com>
2024-08-26 18:41:19 +0800

    Add files via upload

--- src/admission/login/login-script.js
+++ src/admission/login/login-script.js
@@ -5,3 +5,10 @@ function login() {
 function register() {
   alert("Register button clicked!");
 }
+
+function openNav() {
+  document.getElementById("Sidenav").style.width = "250px";
+}
+function closeNav() {
+  document.getElementById("Sidenav").style.width = "0";
+}
